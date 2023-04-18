6d5f1a76f7d1297f92ed2323f15953b2d9bc79d4
PhonSobon <phon.sobon02@gmail.com>
2023-04-18 22:56:18 +0700

    ReatJs-Sobon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import ProductList from './components/ProductList';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div >
+      <Header/>
       <ProductList/>
+      <Footer/>
     </div>
   );
 }
